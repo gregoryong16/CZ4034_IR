fb59833aa2d145da9f7236901ee53be4e2777224
liew312 <56454775+liew312@users.noreply.github.com>
2023-02-23 13:24:26 +0800

    Update merge_files.py

--- Crawling/code/misc/merge_files.py
+++ Crawling/code/misc/merge_files.py
@@ -3,16 +3,18 @@ import pandas as pd
 # csv files in the path
 # file_list = ["shopee_products_Computers-Peripherals-cat.11013247.xlsx","shopee_products_Home-Living-cat.11000001.xlsx"]
 # dtype={'shopid': str,'itemid':str, 'rating':str, 'items_sold':str}
-
-file_list =["shopee_shops_Computers-Peripherals-cat.11013247.xlsx","shopee_shops_Home-Living-cat.11000001.xlsx"]
-dtype={'shopid': str,'userid':str}
+categories = ["Computers-Peripherals-cat.11013247","Home-Living-cat.11000001","Beauty-Personal-Care-cat.11012301","Home-Appliances-cat.11027421","Mobile-Gadgets-cat.11013350"]
+# file_list =["shopee_shops_Computers-Peripherals-cat.11013247.xlsx","shopee_shops_Home-Living-cat.11000001.xlsx"]
+dtype={'shopid': str,'itemid':str, 'rating':str, 'items_sold':str,'userid':str}
+# dtype={'shopid': str,'userid':str}
 # list of excel files we want to merge.
 # pd.read_excel(file_path) reads the excel
 # data into pandas dataframe.
 excl_list = []
-
-for file in file_list:
-    excl_list.append(pd.read_excel(file, dtype=dtype))
+prefix = "shopee_products"
+for category in categories:
+    file = f"{prefix}_{category}.xlsx"
+    excl_list.append(pd.read_excel(file,dtype=dtype))
  
 # create a new dataframe to store the
 # merged excel file.
@@ -28,4 +30,4 @@ for excl_file in excl_list:
 # exports the dataframe into excel file with
 # specified name.
 # excl_merged = excl_merged.dropna(subset=['comment'])
-excl_merged.to_excel('shopee_shops.xlsx', index=False)
+excl_merged.to_excel(f'{prefix}.xlsx', index=False)
