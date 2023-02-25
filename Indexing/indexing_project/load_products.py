e6af0c1f722cd780e69f4e3f1a4a0c577997faad
gregoryong16 <gregory.ong16@gmail.com>
2023-02-25 17:11:28 +0800

    removed db.sqlite3

--- Indexing/indexing_project/load_products.py
+++ Indexing/indexing_project/load_products.py
@@ -11,20 +11,19 @@ import django
 django.setup()
 from indexing_project.models import Products
 
-df = read_excel("shopee_review_product_shop_combined.xlsx",sheet_name = "Sheet1")
-print(df.head()) # print the first 5 rows
+df = read_excel("../shopee_review_product_shop_combined.xlsx",sheet_name = "Sheet1")
+# print(df.head()) # print the first 5 rows
 
+for dbframe in df.itertuples():
+    print(dbframe[0])
+    obj = Products.objects.create(shop_id = dbframe[1], item_id = dbframe[2],time=dbframe[3],username=dbframe[4],comment=dbframe[5],
+                                  rating_star=dbframe[6], template_tags=dbframe[7],product_url=dbframe[8], name=dbframe[9], 
+                                  original_price=dbframe[10], current_price=dbframe[11],description=dbframe[12], rating=dbframe[13], 
+                                  image_url=dbframe[14],items_sold=dbframe[15],category=dbframe[16], total_rating=dbframe[17], 
+                                  user_id=dbframe[18], shop_place=dbframe[19],shop_location=dbframe[20], item_count=dbframe[21],
+                                  shop_ratings=dbframe[22], shop_response_rate=dbframe[23],shop_name=dbframe[24], 
+                                  shop_response_time=dbframe[25], shop_follower_count=dbframe[26],shop_rating_bad=dbframe[27],
+                                  shop_rating_good=dbframe[28],shop_rating_normal=dbframe[29],shop_username=dbframe[30])          
+    obj.save()
 
-# for dbframe in df.itertuples():
-#     print(dbframe["product.name"])
-#     obj = Products.objects.create(Empcode=dbframe.Empcode,firstName=dbframe.firstName, middleName=dbframe.middleName,
-#                                     lastName=dbframe.lastName, email=dbframe.email, phoneNo=dbframe.phoneNo, address=dbframe.address,
-#                                     gender=dbframe.gender, DOB=dbframe.DOB,salary=dbframe.Salary )           
-    # obj.save()
-# for row in range(1, worksheet.max_row+1):
-#     for dbframe in worksheet[row].itertuples():
-#         print(dbframe.product.name)
-#         # obj = Products.objects.create(name=worksheet[i].value.product.name, description=worksheet[i].value.product.description)          
-#         # obj.save()
-
-# print(Products.objects.filter(name__startswith='SG'))
+print(Products.objects.filter(name__startswith='SG'))
